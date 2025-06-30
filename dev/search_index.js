16776fc5ba53282e02bf6f9b1f9840b3a44fcdb4
Documenter.jl <documenter@juliadocs.github.io>
2025-06-30 14:09:08 +0000

    build based on d2b8ca5

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"#InvariantModels","page":"Home","title":"InvariantModels","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for InvariantModels.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"}]
+}
